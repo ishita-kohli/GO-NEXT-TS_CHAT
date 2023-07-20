514fa23b429a3aee4cfaeb4b18d22325d7037cce
Ishita Kohli <kohli.ishita0302@gmail.com>
2023-07-20 08:28:00 +0530

    fix: add cors back

--- server/router/router.go
+++ server/router/router.go
@@ -3,6 +3,7 @@ package router
 import (
 	"server/internal/user"
 	"server/internal/ws"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -13,7 +14,14 @@ var r *gin.Engine
 func InitRouter(userHandler *user.Handler, wsHandler *ws.Handler) {
 	r = gin.Default()
 
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://go-next-ts-chat.vercel.app"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.POST("/signup", userHandler.CreateUser)
 	r.POST("/login", userHandler.Login)
